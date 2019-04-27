31f76d014ec0dfd91accd2c91122395a164003a3
Филипп Хамицевич <spritefok@gmail.com>
2019-04-28 00:28:14 +0300

    Added functionality for whitelist_lsm hooks

--- security/whitelist/whitelist_lsm.c
+++ security/whitelist/whitelist_lsm.c
@@ -1,17 +1,35 @@
- #include <linux/lsm_hooks.h>
+#include <linux/xattr.h>
+#include <linux/binfmts.h>
+#include <linux/lsm_hooks.h>
+#include <linux/cred.h>
 
- static int steve_bprm_check_security(struct linux_binprm *bprm)
- {
-     printk(KERN_INFO "STEVE LSM check of %s\n", bprm->filename);
-     return 0;
- }
+static int whitelist_bprm_check_security(struct linux_binprm *bprm)
+{
+       const struct task_struct *task = current;
+       kuid_t uid = task->cred->uid;
 
- static struct security_hook_list steve_hooks[] = {
-     LSM_HOOK_INIT(bprm_check_security, steve_bprm_check_security),
- };
+       struct dentry *dentry = bprm->file->f_path.dentry;
+       struct inode *inode = d_backing_inode(dentry);
 
- static void __init steve_init(void)
- {
-     security_add_hooks(steve_hooks, ARRAY_SIZE(steve_hooks), "steve");
-     printk(KERN_INFO "STEVE LSM initialized\n");
- }
+       int size = 0;
+
+       if ( uid.val == 0 )
+          return 0;
+
+       size = __vfs_getxattr(dentry, inode, "security.whitelisted", NULL, 0);
+       if ( size > 0 )
+           return 0;
+
+       printk(KERN_INFO "whitelist LSM check of %s denying access for UID %d [ERRO:%d] \n", bprm->filename, uid.val, size );
+       return -EPERM;
+}
+
+static struct security_hook_list whitelist_hooks[] __lsm_ro_after_init = {
+	LSM_HOOK_INIT(bprm_check_security, whitelist_bprm_check_security),
+};
+
+void __init whitelist_add_hooks(void)
+{
+	security_add_hooks(whitelist_hooks, ARRAY_SIZE(whitelist_hooks), "whitelist");
+	printk(KERN_INFO "whitelist LSM initialized\n");
+}
